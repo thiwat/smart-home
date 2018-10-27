71c3bd0f3291e499caf5406f888ced0139d91da3
thiwat <thiwat.nunt@gmail.com>
2018-10-28 00:54:26 +0700

    Edit login function && Create register function
    - Login function with refresh token store
    - Register function with username password name role

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,9 +1,68 @@
 'use strict'
 const mongoose = require('mongoose')
 const Users = mongoose.model('users')
-const { get, hmset, expire } = require('../../token/cache')
+const { hmset, expire } = require('../../token/cache')
+const hash = require('../../token/hash')
 
 exports.login = async function(req, res) {
-    res.status(200)
-    res.send('ok')
+    let userInput = req.body
+
+    try {
+        var result = await Users.find({ username: userInput.username })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ message: "Out of service" })
+        return
+    }
+
+    if (result.length !== 1) {
+        res.status(404).json({ message: "User not found" })
+        return
+    }
+
+    const user = result[0]
+    const [password, salt] = user.password.split(':')
+    if(hash.password(userInput.password, salt) !== password) {
+        res.status(401).json({ message: "Wrong password" })
+        return
+    }
+    
+    const refreshToken = hash.hmac(userInput.username)
+    await hmset(refreshToken, {name: user.name, role: user.role})
+    await expire(refreshToken, 20 * 1000)
+
+    res.json({ message: "Success", refreshToken })
+}
+
+exports.register = async function(req, res) {
+    let user = new Users(req.body)
+    
+    try {
+        var checkExist = await Users.find({ username: user.username }).countDocuments()
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ message: "Out of service" })
+        return
+    }
+
+    if (checkExist !== 0) {
+        res.status(400).json({ message: "This username is already exist"})
+        return
+    }
+
+    const salt = hash.salt(user.username)
+    const password = hash.password(user.password, salt)
+    const storePassword = `${password}:${salt}`
+
+    user.password = storePassword
+
+    try {
+        await user.save()
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ message: "Out of service "})
+        return
+    }
+    
+    res.json({ message: "Register success."})
 }
--- api/routes/usersRoute.js
+++ api/routes/usersRoute.js
@@ -1,8 +1,13 @@
 'use strict'
 
+const { checkKey } = require('../../token/auth')
+
 module.exports = function(app) {
     const users = require('../controllers/usersController')
     app.route('/login')
-        .post(users.login)
+        .post(checkKey, users.login)
+
+    app.route('/register')
+        .post(checkKey, users.register)
 
 }
